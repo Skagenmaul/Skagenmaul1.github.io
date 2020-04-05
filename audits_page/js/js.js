b63eff645427ca49f7159b02459a0121fec9e53d
Skagenmaul <Skagenmaul@users.noreply.github.com>
2020-04-05 23:46:10 +0200

    t

--- audits_page/js/js.js
+++ audits_page/js/js.js
@@ -1,13 +1,5 @@
 
-// background.js
-browser.runtime.onMessage.addListener(message => {
-  console.log("background: onMessage", message);
 
-  // Add this line:
-  return Promise.resolve("Dummy response to keep the console quiet");
-});
-
-console.log("test");
 $("a").click(function(){
 
 
