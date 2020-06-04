50da593d37926a1f585bde1a9df3c5e8a8d8a187
Skagenmaul <Skagenmaul@users.noreply.github.com>
2020-06-05 01:12:21 +0200

    f

--- Slutuppgift/js/javascript.js
+++ Slutuppgift/js/javascript.js
@@ -1,3 +1,20 @@
 var dt = new Date();
 
 document.getElementById("datetime").innerHTML = dt.toLocaleDateString();
+
+
+// Irrelevant kod då detta redan fungerar via css. Kan fundera på utveckling. Kan användas till att stanna menyn längre ner på sidan.
+// When the user scrolls the page, execute myFunction
+// window.onscroll = function() {myFunction()};
+// Get the navbar
+// var headern = document.getElementById("headern");
+// Get the offset position of the navbar
+// var sticky = headern.offsetTop;
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     headern.classList.add("sticky")
+//   } else {
+//     headern.classList.remove("sticky");
+//   }
+// }
